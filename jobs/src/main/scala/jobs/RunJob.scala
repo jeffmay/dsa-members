48587a73925d888b878ccfb79b7105d53796bd1f
Jeff May <jeff.n.may@gmail.com>
2021-04-04 13:48:12 -0700

    RunJob changes

--- jobs/src/main/scala/jobs/RunJob.scala
+++ jobs/src/main/scala/jobs/RunJob.scala
@@ -1,6 +1,8 @@
 package org.dsasf.members
 package jobs
 
+import jobs.national.ImportNationalMembership
+
 import zio._
 import zio.console.Console
 
@@ -8,7 +10,11 @@ object RunJob extends App {
 
   override def run(args: List[String]): URIO[ZEnv, ExitCode] = {
     for {
-      _ <- ZIO.accessM[Console](_.get.putStrLn("Hello"))
+      console ← ZIO.service[Console.Service]
+//      _ ← console.putStrLn("Hello. What file do you want to process?")
+//      filename ← console.getStrLn.orDie
+      count ← ImportNationalMembership.fromCSV("tmp/dsasf-2021-03-21.csv").orDie
+      _ ← console.putStrLn(s"Total records parsed: $count")
     } yield ExitCode.success
   }
 }
